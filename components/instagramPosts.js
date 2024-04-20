5814fddf95c2fbd6cebc5367480682dbb3dd80f9
ocean6954 <oceanan699@gmail.com>
2024-04-21 04:44:01 +0900

    [update]投稿10個表示と正方形化

--- components/instagramPosts.js
+++ components/instagramPosts.js
@@ -7,24 +7,22 @@ import reelIcon from 'images/icon/reel.png'
 const InstagramPosts = ({ posts }) => {
   return (
     <div className={styles.postWrapper}>
-      {posts.business_discovery.media.data.map((media, index) => {
-        {
-          media.media_type === 'VIDEO' && (
-            <div className={styles.card}>
-              <div className={styles.media}>
-                <div className={styles.iconContainer}>
-                  <Image src={reelIcon} layout="responsive" width="100%" />
-                </div>
-                <Image
-                  src={media.thumbnail_url}
-                  alt=""
-                  fill
-                  priority
-                  blurDataURL="data:image/png;base64,..."
-                  className={styles.image}
-                />
-                {/* <button onClick={toggleVideo}>Click</button> */}
+      {posts.business_discovery.media.data.map((media, index) => (
+        <>
+          {media.media_type === 'VIDEO' && (
+            <div className={styles.media}>
+              <div className={styles.iconContainer}>
+                <Image src={reelIcon} layout="responsive" width="100%" />
               </div>
+              <Image
+                src={media.thumbnail_url}
+                alt=""
+                fill
+                priority
+                blurDataURL="data:image/png;base64,..."
+                className={styles.image}
+              />
+              {/* <button onClick={toggleVideo}>Click</button> */}
               {/* {showVideo && (
                   <ReactPlayer
                     url={media.media_url} // 相対パスを指定
@@ -34,47 +32,38 @@ const InstagramPosts = ({ posts }) => {
                   />
                 )} */}
             </div>
-          )
-        }
-        {
-          media.media_type === 'IMAGE' && (
-            <div className={styles.card}>
-              <div className={styles.media}>
-                <Image
-                  src={media.media_url}
-                  alt=""
-                  fill
-                  priority
-                  blurDataURL="data:image/png;base64,..."
-                  className={styles.image}
-                />
-                {/* <button onClick={toggleVideo}>Click</button> */}
-              </div>
+          )}
+          {media.media_type === 'IMAGE' && (
+            <div className={styles.media}>
+              <Image
+                src={media.media_url}
+                alt=""
+                fill
+                priority
+                blurDataURL="data:image/png;base64,..."
+                className={styles.image}
+              />
+              {/* <button onClick={toggleVideo}>Click</button> */}
             </div>
-          )
-        }
-        {
-          media.media_type === 'CAROUSEL_ALBUM' && (
-            <div className={styles.card}>
-              <Splide aria-label="インスタグラムの投稿">
-                {media.children.data.map((child) => (
-                  <SplideSlide className={styles.media}>
-                    <Image
-                      src={child.media_url}
-                      alt="インスタグラムの投稿です"
-                      fill
-                      priority
-                      blurDataURL="data:image/png;base64,..."
-                      className={styles.image}
-                    />
-                  </SplideSlide>
-                ))}
-              </Splide>
-              <p className={styles.caption}>{media.caption}</p>
-            </div>
-          )
-        }
-      })}
+          )}
+          {media.media_type === 'CAROUSEL_ALBUM' && (
+            <Splide aria-label="インスタグラムの投稿">
+              {media.children.data.map((child) => (
+                <SplideSlide className={styles.media}>
+                  <Image
+                    src={child.media_url}
+                    alt="インスタグラムの投稿です"
+                    fill
+                    priority
+                    blurDataURL="data:image/png;base64,..."
+                    className={styles.image}
+                  />
+                </SplideSlide>
+              ))}
+            </Splide>
+          )}
+        </>
+      ))}
     </div>
   )
 }
