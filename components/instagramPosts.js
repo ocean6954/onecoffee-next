cdd053a73b52708c9a2ec7b886e8b20097fe1e31
ocean6954 <oceanan699@gmail.com>
2024-05-13 19:13:24 +0900

    [update]インスタグラムポストのレスポンシブレイアウトの実装と調整

--- components/instagramPosts.js
+++ components/instagramPosts.js
@@ -1,35 +1,37 @@
-import React, { useEffect, useState, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import styles from 'styles/instagramPosts.module.css'
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import Image from 'next/image'
-import reelIcon from 'images/icon/reel.png'
+// import reelIcon from 'images/icon/reel.png'
 
 const InstagramPosts = ({ posts }) => {
   return (
-    <div className={styles.postWrapper}>
-      {posts.business_discovery.media.data.map((media) => (
-        <Fragment key={media.id}>
-          {/* {console.log(`media情報です${media}`)} */}
-          {media.media_type === 'VIDEO' && (
-            <div className={styles.media}>
-              <div className={styles.iconContainer}>
+    <>
+      <h2 className={styles.head2}>Instagram</h2>
+      <div className={styles.postWrapper}>
+        {posts.business_discovery.media.data.map((media) => (
+          <Fragment key={media.id}>
+            {/* {console.log(`media情報です${media}`)} */}
+            {media.media_type === 'VIDEO' && (
+              <div className={styles.media}>
+                {/* <div className={styles.iconContainer}>
                 <Image
                   src={reelIcon}
                   layout="responsive"
                   width="100%"
                   alt="動画アイコン"
                 />
-              </div>
-              <Image
-                src={media.thumbnail_url}
-                alt="インスタグラムの投稿です"
-                fill
-                priority
-                blurDataURL="data:image/png;base64,..."
-                className={styles.image}
-              />
-              {/* <button onClick={toggleVideo}>Click</button> */}
-              {/* {showVideo && (
+              </div> */}
+                <Image
+                  src={media.thumbnail_url}
+                  alt="インスタグラムの投稿です"
+                  fill
+                  priority
+                  blurDataURL="data:image/png;base64,..."
+                  className={styles.image}
+                />
+                {/* <button onClick={toggleVideo}>Click</button> */}
+                {/* {showVideo && (
                   <ReactPlayer
                     url={media.media_url} // 相対パスを指定
                     controls
@@ -37,40 +39,41 @@ const InstagramPosts = ({ posts }) => {
                     height="100%"
                   />
                 )} */}
-            </div>
-          )}
-          {media.media_type === 'IMAGE' && (
-            <div className={styles.media}>
-              <Image
-                src={media.media_url}
-                alt="インスタグラムの投稿です"
-                fill
-                priority
-                blurDataURL="data:image/png;base64,..."
-                className={styles.image}
-              />
-              {/* <button onClick={toggleVideo}>Click</button> */}
-            </div>
-          )}
-          {media.media_type === 'CAROUSEL_ALBUM' && (
-            <Splide aria-label="インスタグラムの投稿">
-              {media.children.data.map((child) => (
-                <SplideSlide className={styles.media} key={child.id}>
-                  <Image
-                    src={child.media_url}
-                    alt="インスタグラムの投稿です"
-                    fill
-                    priority
-                    blurDataURL="data:image/png;base64,..."
-                    className={styles.image}
-                  />
-                </SplideSlide>
-              ))}
-            </Splide>
-          )}
-        </Fragment>
-      ))}
-    </div>
+              </div>
+            )}
+            {media.media_type === 'IMAGE' && (
+              <div className={styles.media}>
+                <Image
+                  src={media.media_url}
+                  alt="インスタグラムの投稿です"
+                  fill
+                  priority
+                  blurDataURL="data:image/png;base64,..."
+                  className={styles.image}
+                />
+                {/* <button onClick={toggleVideo}>Click</button> */}
+              </div>
+            )}
+            {media.media_type === 'CAROUSEL_ALBUM' && (
+              <Splide aria-label="インスタグラムの投稿">
+                {media.children.data.map((child) => (
+                  <SplideSlide className={styles.media} key={child.id}>
+                    <Image
+                      src={child.media_url}
+                      alt="インスタグラムの投稿です"
+                      fill
+                      priority
+                      blurDataURL="data:image/png;base64,..."
+                      className={styles.image}
+                    />
+                  </SplideSlide>
+                ))}
+              </Splide>
+            )}
+          </Fragment>
+        ))}
+      </div>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <InstagramComponent />
+
       <Hero />
       <Review />
       <MenuInformation />
       <Container>
         {/* <Beans /> */}
         <Information />
+        <InstagramComponent />
       </Container>
 
       {/* <Beans /> */}
